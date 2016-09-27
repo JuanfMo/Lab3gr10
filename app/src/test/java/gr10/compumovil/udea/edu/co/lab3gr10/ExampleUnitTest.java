c3f4fd28ebcd42225945214f489825c19b0ec178
JuanfMo <jufemorx_15@hotmail.com>
2016-09-26 20:34:22 -0500

    Laboratorio 3
    weather app

---
+++ app/src/test/java/gr10/compumovil/udea/edu/co/lab3gr10/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package gr10.compumovil.udea.edu.co.lab3gr10;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
